8dcc380226ce490811e1e73d90bbb1a200f79410
avdosev <avdosev1@yandex.ru>
2020-08-20 23:27:33 +0400

    Рабочий минимум

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,13 @@
 from game_info import GameData
 from dataclasses import asdict
+import telepot
 
 message = "Игрулька {name} доступна бесплатно [тут]({link})"
 
 
 class TelegramBot:
     def __init__(self, api_key, chat_id):
-        self.api_key = api_key
+        self.bt = telepot.Bot(api_key)
         self.chat_id = chat_id
         print(f"bot inited with: api_key={api_key}; chat_id={chat_id};")
 
@@ -14,4 +15,4 @@ class TelegramBot:
         print("send response telegram")
         mes = message.format(**asdict(game))
         print("Message:", mes)
-        pass
+        self.bt.sendPhoto(self.chat_id, game.image_url, caption=mes)
